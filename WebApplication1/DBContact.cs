b22d088005954997b963383d09a4454c82090621
Myp <kirill.myp@yandex.ru>
2016-03-21 01:56:26 +0300

    all

---
+++ WebApplication1/DBContact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class DBContact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string ThirdName { get; set; }
+        public string IdTelephone { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class DBTelephones
+    {
+        public int Id { get; set; }
+        public string Telephone_Number { get; set; }
+    }
+}
---
+++ WebApplication1/MyWS.asmx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Runtime.Serialization.Json;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Сводное описание для MyWS
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Чтобы разрешить вызывать веб-службу из скрипта с помощью ASP.NET AJAX, раскомментируйте следующую строку. 
+    [System.Web.Script.Services.ScriptService]
+    public class MyWS : System.Web.Services.WebService
+    {
+        private readonly MyDC2DataContext dc2 = new MyDC2DataContext();   
+        [WebMethod]
+        public DBContact GetContactById(string contactId)
+        {
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            
+
+            var contact = from result in dc2.Contacts
+                          where result.Id == int.Parse(contactId)
+                          select result;
+            string resultString = jss.Serialize(contact);//для запроса по таблице контактов
+
+            List<DBContact> listContact = (List<DBContact>)jss.Deserialize(resultString, typeof(List<DBContact>));
+            if (listContact.Count == 0)//учитываем что возможно контакты пусты
+            {
+                listContact.Add(new DBContact
+                {
+                    Id = 0,
+                    ThirdName = "empty",
+                    SecondName = "empty",
+                    FirstName = "empty",
+                    IdTelephone = "empty",
+                    Address = "empty"
+                });
+                return listContact[0];
+            }
+
+            var telephone = from result in dc2.Telephones
+                            where result.Id == int.Parse(listContact[0].IdTelephone)
+                            select result;
+            resultString = jss.Serialize(telephone);//для запроса по таблице номеров
+            List<DBTelephones> listTeeTelephones = (List<DBTelephones>)jss.Deserialize(resultString, typeof(List<DBTelephones>));
+            
+            if (listTeeTelephones.Count == 0)
+            {
+                listContact[0].IdTelephone = "empty";
+            }
+            else
+            {
+                listContact[0].IdTelephone = listTeeTelephones[0].Telephone_Number;
+            }
+            return listContact[0];
+        }
+    }
+}
